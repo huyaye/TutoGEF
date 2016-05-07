cb612aee9a4bf145f8f428c700f8b6fd16584478
Jungwook <jungwook.ryu@samsung.com>
2016-05-07 20:24:00 +0900

    Add EditPart for Enterprise (p.16)

---
+++ src/tutogef/editpart/EnterprisePart.java
@@ -0,0 +1,40 @@
+package tutogef.editpart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
+
+import tutogef.figure.EnterpriseFigure;
+import tutogef.model.Enterprise;
+import tutogef.model.Node;
+
+public class EnterprisePart extends AbstractGraphicalEditPart {
+
+	@Override
+	protected IFigure createFigure() {
+		IFigure figure = new EnterpriseFigure();
+		return figure;
+	}
+
+	@Override
+	protected void createEditPolicies() {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	protected void refreshVisuals() {
+		EnterpriseFigure figure = (EnterpriseFigure)getFigure();
+		Enterprise model = (Enterprise)getModel();
+		
+		figure.setName(model.getName());
+		figure.setAddress(model.getAddress());
+		figure.setCapital(model.getCapital());
+	}
+
+	@Override
+	public List<Node> getModelChildren() {
+		return new ArrayList<Node>();
+	}
+}
